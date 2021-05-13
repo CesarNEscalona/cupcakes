b66376b058d2b670e8e0fa45217000f1825da276
Phillip Ball <pball@mail.greenriver.edu>
2021-05-13 13:30:30 -0400

    Created a data layer in the model folder.

---
+++ model/data-layer.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * dataLayer.php
+ * Return data for the cupcakes
+ */
+
+function getFlavors()
+{
+    return array("grasshopper" => "The Grasshopper",
+        "maple" => "Whiskey Maple Bacon",
+        "carrot" => "Carrot Walnut",
+        "caramel" => "Salted Caramel Cupcake",
+        "velvet" => "Red Velvet",
+        "lemon" => "Lemon Drop",
+        "tiramisu" => "Tiramisu",);
+}
