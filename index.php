f4381b1b22c229dda779f4dfd1b80bb4294b538f
Cesar Escalona <cesarescalona10@gmail.com>
2021-05-13 14:01:40 -0400

    Updated index page with flavors and created home page

--- index.php
+++ index.php
@@ -10,7 +10,33 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET /', function(){
+$f3->route('GET|POST /', function ($f3) {
+    //Reinitialize session array
+    $_SESSION = array();
+
+    // initialize variables to store user input
+    $userFlavors = array();
+
+    //If the form has been submitted, validate the data
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        // Get user input and assign it to the variable
+        $userFlavors = $_POST['flavors'];
+        //If condiments are valid
+        if (validFlavors($userFlavors)) {
+            $_SESSION['flavors'] = implode(", ", $userFlavors);
+        } else {
+            $f3->set('errors["flavors"]', 'Invalid selection');
+        }
+        //If the error array is empty, redirect to summary page
+        if (empty($f3->get('errors'))) {
+            header('location: summary');
+        }
+    }
+
+    // Add the data to the hive
+    $f3->set('userFlavors', $userFlavors);
+
     // Display the home page
     $view = new Template();
     echo $view->render('views/home.html');
